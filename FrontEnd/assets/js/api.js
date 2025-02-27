dbd3bf103a394dd3acacd8beb8ae8b3dca88ab7e
SSVGLeo <troupleo@gmail.com>
2025-02-27 14:44:28 +0100

    Fin des étapes 1 : Création des projets dynamique avec le back Création des boutons avec le back Filtrage des catégories des images avec les boutons

--- FrontEnd/assets/js/api.js
+++ FrontEnd/assets/js/api.js
@@ -12,10 +12,51 @@ export function fetchWorks(projets) {
     });
 }
 
+export function displayFilters(projets) {
+    const filters = document.querySelector(".filters");
+
+    const categoriesMap = new Map();
+
+    projets.forEach((projet) => {
+        categoriesMap.set(projet.category.name, projet.category.id);
+    });
+
+    categoriesMap.forEach((id,name) => {
+        const button = document.createElement("button");
+        button.textContent = `${name}`;
+        button.dataset.id = `${id}`;
+        filters.appendChild(button);
+    })
+}
+
+
+export function setupFilters(projets) {
+    const buttons = document.querySelectorAll(".filters button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const categoryId = button.dataset.id;
+
+            buttons.forEach((button) => button.classList.remove("selected"));
+            button.classList.add("selected");
+
+            let filteredProjets;
+            if (categoryId === undefined) {
+                filteredProjets = projets;
+            } else {
+                filteredProjets = projets.filter(projet => projet.category.id === parseInt(categoryId));
+            }
+
+            fetchWorks(filteredProjets);
+    })
+    })
+}
+
 fetch("http://localhost:5678/api/works")
   // On va chercher les valeurs de l'api et on les lit en json
   .then((response) => response.json())
   .then((projets) => {
     fetchWorks(projets);
+    displayFilters(projets);
+    setupFilters(projets)
   })
   .catch((error) => console.log(error));
--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,11 +1,15 @@
-import {fetchWorks} from './js/api.js';
+import {fetchWorks, displayFilters} from './js/api.js';
 // import {displayWorks} from './js/gallery.js';
 // import {setupLogin} from './js/authentification.js';
 // import {setupModal} from './js/modal.js';
 
 console.log("Js bien importé");
 
+displayFilters();
+setupFilters();
 fetchWorks();
+
+
 // displayWorks();
 // setupLogin();
 // setupModal();
