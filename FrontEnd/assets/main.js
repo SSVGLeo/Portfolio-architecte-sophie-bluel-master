aabe544acf9bd19c80e47de8d8eaa9fef35f3451
SSVGLeo <troupleo@gmail.com>
2025-03-03 11:06:25 +0100

    Changement de l'organisation du js du login pour qu'il s'exporte dans main.js pour une meilleure organisation

--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,16 +1,14 @@
 import {fetchWorks, displayFilters} from './js/api.js';
-// import {displayWorks} from './js/gallery.js';
-// import {setupLogin} from './js/authentification.js';
+import {setupLogin} from './js/authentification.js';
 // import {setupModal} from './js/modal.js';
 
 console.log("Js bien importé");
+
+setupLogin();
 
 displayFilters();
 setupFilters();
 fetchWorks();
 
-// setupLogin();
-
-// displayWorks();
 
 // setupModal();
